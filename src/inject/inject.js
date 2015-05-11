b4bf6427fa7cba87657f0471ca5174ea5e6cbd89
Sam Sandberg <sam.sandberg@gmail.com>
2015-05-11 17:51:53 -0400

    And dat spacing

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,19 +1,19 @@
 chrome.extension.sendMessage({}, function(response) {
   var readyStateCheckInterval = setInterval(function() {
-  if (document.readyState === "complete") {
-    clearInterval(readyStateCheckInterval);
-    // ----------------------------------------------------------
-    // This part of the script triggers when page is done loading
-    //console.log("Hello. This message was sent from scripts/inject.js");
-    // ----------------------------------------------------------
-    var numChanged = 0;
-    $('time').each(function () {
-      var timeStr = $(this).attr('title');
-      $(this).after(timeStr);
-      $(this).remove();
-      numChanged++;
-    });
-    console.log("Modified " + numChanged + " Github timestamps");
-  }
+    if (document.readyState === "complete") {
+      clearInterval(readyStateCheckInterval);
+      // ----------------------------------------------------------
+      // This part of the script triggers when page is done loading
+      //console.log("Hello. This message was sent from scripts/inject.js");
+      // ----------------------------------------------------------
+      var numChanged = 0;
+      $('time').each(function () {
+        var timeStr = $(this).attr('title');
+        $(this).after(timeStr);
+        $(this).remove();
+        numChanged++;
+      });
+      console.log("Modified " + numChanged + " Github timestamps");
+    }
   }, 10);
 });
